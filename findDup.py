9561eb50a65b91b70d20baac0140c2655c065587
Stephen M. Clark <steve@clarkhome.org>
2015-03-19 23:23:51 -0500

    Added error handling.

--- findDup.py
+++ findDup.py
@@ -4,22 +4,26 @@ import fileUtils
 def getHashes(rootPath):
    result = dict()
    files = fileUtils.recurse(rootPath)
+   badFiles = []
    for f in files:
-      md5 = fileUtils.md5(f)
-      print "{:25s} : {:s}".format(md5, f)
-      if md5 in result:
-         # replace
-         l = result[md5]
-         l.append(f)
-         result[md5] = l
-      else:
-         l = []
-         l.append(f)
-         result[md5] = l
-   return result
+      try:
+         md5 = fileUtils.md5(f)
+         print "{:25s} : {:s}".format(md5, f)
+         if md5 in result:
+            # replace
+            l = result[md5]
+            l.append(f)
+            result[md5] = l
+         else:
+            l = []
+            l.append(f)
+            result[md5] = l
+      except:
+         badFiles.append(f)
+   return result, badFiles
 
 if __name__ == "__main__":
-   hashes = getHashes(os.getcwd())
+   hashes, badFiles = getHashes(os.getcwd())
    with open("dupResults.txt", "w") as of:
       for k in hashes:
          items = hashes[k]
@@ -29,3 +33,7 @@ if __name__ == "__main__":
             for i in items:
                print "     {:s}".format(i)
                print >>of, "     {:s}".format(i)
+      if len(badFiles):
+         print >>of, "Bad Files:"
+         for f in badFiles:
+            print >>of, "     {:s}".format(f)
